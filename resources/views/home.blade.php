1056434a2e4ff5f6f8fd7ebc33b03c115f5a802c
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-03-29 10:15:17 +0100

    Added Home page
    Replaced Laravel default welcome view with my own homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,239 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+    <title>Homepage</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+    
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">System</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Panel</a></li>
+            <li><a href="#">Feature 1</a></li>
+            <li><a href="#">Feature 2</a></li>
+            <li><a href="#">Feature 3</a></li>
+            <li><a href="#">Support</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Welcome, $user</a></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <header id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10">
+                    <h1><span class="glyphicon glyphicon-home" aria-hidden="true"></span> System <small>Development mode</small></h1>
+                </div>
+                <!--<div class="col-md-2">
+                    <div class="dropdown create">
+                      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Create content
+                        <span class="caret"></span>
+                      </button>
+                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">Action 1</a></li>
+                        <li><a href="#">Action 2</a></li>
+                        <li><a href="#">Action 3</a></li>
+                      </ul>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </header>
+
+    <section id="breadcrumb">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="active">Home</li>
+            </ol>
+        </div>
+    </section>
+
+    <section id="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item active main-color-bg title">
+                            Navigation
+                        </a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Link</a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Link</a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Link <span class="badge">15</span></a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Link <span class="badge">56</span></a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Link <span class="badge">1,905</span></a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Link</a>
+                        <a href="#" class="list-group-item"><strong>Admin section</strong></a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Admin link</a>
+                        <a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Admin link</a>
+
+                    </div>
+                    <div class="well well-progress-bars">
+                        <h4>Frontend Completion</h4>
+                        <div class="progress">
+                          <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+                            30%
+                          </div>
+                        </div>
+                        <h4>Backend Completion</h4>
+                        <div class="progress">
+                          <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
+                            10%
+                          </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Main body of content -->
+
+                <div class="col-md-9">
+
+                    <!-- Featured items -->
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading main-color-bg">
+                            <h3 class="panel-title">Statistics</h3>
+                         </div>
+                         <div class="panel-body">
+
+                            <div class="col-md-3">
+                                <div class="well featured-items">
+                                    <h2><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> 127</h2>
+                                    <h4>Panels created</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="well featured-items">
+                                    <h2><span class="glyphicon glyphicon-alert" aria-hidden="true"></span> 2,954</h2>
+                                    <h4>Panel errors</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="well featured-items">
+                                    <h2><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> 886</h2>
+                                    <h4>Feature 2</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="well featured-items">
+                                    <h2><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> 12</h2>
+                                    <h4>Projects expired</h4>
+                                </div>
+                            </div>
+
+                         </div>
+                    </div>
+
+                    <!-- Latest projects -->
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading main-color-bg">
+                            <h3 class="panel-title">Project overview</h3>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table table-striped table-hover centre-text">
+                                <tr>
+                                    <th>PROJECT ID</th>
+                                    <th>TYPE</th>
+                                    <th>PROVIDER</th>
+                                    <th>LINK</th>
+                                    <th>OWNER</th>
+                                    <th>STATUS</th>
+                                    <th>VIEW</th>
+                                </tr>
+                                <tr>
+                                    <th>1</th>
+                                    <th>Panel</th>
+                                    <th>Test Provider</th>
+                                    <th>http://google.com</th>
+                                    <th>Dan</th>
+                                    <th class="font-green">LIVE</th>
+                                    <th><button type="button" class="btn btn-danger">View</button></th>
+                                </tr>
+                                <tr class="row-offset">
+                                    <th>2</th>
+                                    <th>Panel</th>
+                                    <th>Test Provider</th>
+                                    <th>http://google.com</th>
+                                    <th>Dan</th>
+                                    <th class="font-orange">PAUSED</th>
+                                    <th><button type="button" class="btn btn-danger">View</button></th>
+                                </tr>
+                                <tr>
+                                    <th>3</th>
+                                    <th>Panel</th>
+                                    <th>Test Provider</th>
+                                    <th>http://google.com</th>
+                                    <th>Dan</th>
+                                    <th class="font-red">OFFLINE</th>
+                                    <th><button type="button" class="btn btn-danger">View</button></th>
+                                </tr>
+                                <tr class="row-offset">
+                                    <th>4</th>
+                                    <th>Panel</th>
+                                    <th>Test Provider</th>
+                                    <th>http://google.com</th>
+                                    <th>Dan</th>
+                                    <th class="font-green">LIVE</th>
+                                    <th><button type="button" class="btn btn-danger">View</button></th>
+                                </tr>
+                                <tr>
+                                    <th>5</th>
+                                    <th>Panel</th>
+                                    <th>Test Provider</th>
+                                    <th>http://google.com</th>
+                                    <th>Dan</th>
+                                    <th class="font-green">LIVE</th>
+                                    <th><button type="button" class="btn btn-danger">View</button></th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- End of main body of content -->
+
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--<script src="{{ URL::asset('js/jquery.min.js') }}"></script>--> 
+    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+
+  </body>
+</html>
