5c7bb52633a607f88608264e5b4dc06783c13066
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-05-02 12:20:40 +0100

    Added respondents table migration

---
+++ database/migrations/2018_05_01_151752_create_respondents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRespondentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('respondents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('panel_id');
+            $table->string('ip_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('respondents');
+    }
+}
