e4bc92ff47f545b363ebd7aca123fdac52bb6c9a
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-05-09 15:12:59 +0100

    Updated projects page to be dynamic

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -45,72 +45,74 @@
               </tr>
               <tr>
                 <td width="15%" class="active"><b>Company Contact</b></td>
-                <td width="35%" colspan="2"><a href="mailto:#">{{ $project->user->email }}</a></td>
+                <td width="35%" colspan="2"><a href="mailto:{{ $project->user->email }}">{{ $project->user->email }}</a></td>
                 <td width="15%" class="active"><b>Client Contact</b></td>
-                <td width="35%"><a href="mailto:#">client.example@email.com</a></td>
+                <td width="35%"><a href="mailto:{{ $project->clientEmail }}">{{ $project->clientEmail }}</a></td>
               </tr>
               <tr>
                 <td width="15%" class="active"><b>Methodology</b></td>
-                <td width="35%" colspan="2">Online</td>
+                <td width="35%" colspan="2">{{ $project->methodology }}</td>
                 <td width="15%" class="active"><b>Total Interviews</b></td>
-                <td width="35%">1000</td>
+                <td width="35%">{{ $project->totalInterviews }}</td>
               </tr>
               <tr>
                 <td width="15%" class="active"><b>Project Files</b></td>
                 <td width="35%" colspan="2"><a href="#">Click here to open</a></td>
                 <td width="20%" class="active"><b>Amount Quoted</b></td>
-                <td width="30%">£20,000</td>
+                <td width="30%">{{ $project->amountQuoted }}</td>
               </tr>
               <tr>
                 <td colspan="5" align="center" class="panel-heading main-color-bg"><b>Project timeline</b></td>
               </tr>
               <tr>
                 <td width="15%" class="active"><b>Questionnaire by</b></td>
-                <td width="35%" colspan="2">01/01/1990</td>
+                <td width="35%" colspan="2">{{ $project->questionnaireBy }}</td>
                 <td width="15%" class="active"><b>Scripting Complete</b></td>
-                <td width="35%">01/01/1990</td>
+                <td width="35%">{{ $project->scriptedBy }}</td>
               </tr>
               <tr>
                 <td width="15%" class="active"><b>Fieldwork Start</b></td>
-                <td width="35%" colspan="2">01/01/1990</td>
+                <td width="35%" colspan="2">{{ $project->fieldStart }}</td>
                 <td width="15%" class="active"><b>Fieldwork End</b></td>
-                <td width="35%">01/01/1990</td>
+                <td width="35%">{{ $project->fieldEnd }}</td>
               </tr>
               <tr>
                 <td width="20%" class="active"><b>Data spec by</b></td>
-                <td width="30%" colspan="2">01/01/1990</td>
+                <td width="30%" colspan="2">{{ $project->dataSpecBy }}</td>
                 <td width="20%" class="active"><b>Final Data by</b></td>
-                <td width="30%">01/01/1990</td>
+                <td width="30%">{{ $project->finalDataBy }}</td>
               </tr>
               <tr>
-                <td colspan="5" align="center" class="panel-heading main-color-bg"><b>Project panel</b></td>
+                <td colspan="5" align="center" class="panel-heading main-color-bg"><b>Project panels</b></td>
               </tr>
               <tr>
+                @foreach ($project->panels as $panel)
+                <td width="15%" class="active"><b>Panel name</b></td>
+                <td width="35%" colspan="2" class="font-green">{{ $panel->panelName }}</td>
                 <td width="15%" class="active"><b>Panel status</b></td>
-                <td width="35%" colspan="2" class="font-green">LIVE</td>
-                <td width="15%" class="active"><b>View panel details</b></td>
-                <td width="35%"><a href="#">Click here to open</a></td>
+                <td width="35%" colspan="2" class="font-green">{{ $panel->status }}</td>
+                @endforeach
               </tr>
               <tr>
                 <td colspan="5" align="center" class="panel-heading main-color-bg"><b>Project deliverables</b></td>
               </tr>
               <tr>
                 <td width="20%" class="active"><b>Open Questions</b></td>
-                <td width="30%" colspan="2">3</td>
+                <td width="30%" colspan="2">{{ $project->openQuestions }}</td>
                 <td width="20%" class="active"><b>Codeframe</b></td>
-                <td width="30%">Yes</td>
+                <td width="30%">{{ $project->codeframeRequired }}</td>
               </tr>
               <tr>
                 <td width="20%" class="active"><b>Raw data file</b></td>
-                <td width="30%" colspan="2">SPSS</td>
+                <td width="30%" colspan="2">{{ $project->rawDataFormat }}</td>
                 <td width="20%" class="active"><b>Cross Tabs</b></td>
-                <td width="30%">Yes</td>
+                <td width="30%">{{ $project->crossTabsRequired }}</td>
               </tr>
               <tr>
                 <td width="20%" class="active"><b>Tab Format</b></td>
-                <td width="30%" colspan="2">Excel</td>
+                <td width="30%" colspan="2">{{ $project->tabFormat }}</td>
                 <td width="20%" class="active"><b>Verb Format</b></td>
-                <td width="30%">Excel</td>
+                <td width="30%">{{ $project->verbFormat }}</td>
               </tr>
               <tr>
                 
@@ -120,7 +122,7 @@
               </tr>
               <tr>
                 <td width="20%" class="active"><b>Notes</b></td>
-                <td colspan="4">No additional information has been entered</td>
+                <td colspan="4">{{ $project->notes }}</td>
               </tr>
             </tbody>
         </table>
