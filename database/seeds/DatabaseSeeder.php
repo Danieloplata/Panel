3aae3cb8b1cfd2c91c1d0e2eb57545c93f0a75ce
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-07-29 16:33:10 +0100

    Added countries seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     		UsersTableSeeder::class,
     		ProjectsTableSeeder::class,
     		PanelsTableSeeder::class,
+            CountriesTableSeeder::class,
     	]);
     }
 }
