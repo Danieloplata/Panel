018cf0d4bbeceae8a347a09e03a25ba3362b60d4
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-07-31 18:20:41 +0100

    Added basic Provider views

---
+++ resources/views/providers/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts/master')
+
+@section('pagetitle')
+    Providers overview
+@endsection
+
+@section('headerDropDown')
+    
+@endsection
+
+@section('breadcrumb')
+    Home > Providers > Providers overview
+@endsection
+
+@section('content')
+    <!-- Latest Providers -->
+
+    <div class="panel panel-default">
+        <div class="panel-heading main-color-bg">
+            <h3 class="panel-title">Providers overview</h3>
+        </div>
+        <div class="panel-body">
+            <table class="table table-striped table-hover centre-text">
+                <tr>
+                    <th>ID</th>
+                    <th>Provider</th>
+                    <th></th>
+                </tr>
+
+                @foreach ($providers as $provider)
+                <tr>
+                    <th class="vertical-align">{{ $provider->id }}</th>
+                    <th>{{ $provider->projectName }}</th>
+                    <th><a href="{{ url('/providers') }}/{{ $provider->id }}"><button type="button" class="btn btn-danger">View</button></a></th>
+                </tr>
+                @endforeach
+
+            </table>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/providers/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts/master')
+
+@section('pagetitle')
+    Homepage
+@endsection
+
+@section('headerDropDown')
+    
+@endsection
+
+@section('breadcrumb')
+    Home > Providers > Displaying provider: {{ $provider->providerName }}
+@endsection
+
+@section('content')
+
+@endsection
