8ef0a5ffe0f505ccb35348000c6bc0265bd3883e
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-09-25 11:21:14 +0100

    Updated UsersTableSeeder with Faker

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,10 +12,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+
         DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@email.com',
-            'password' => bcrypt('secret'),
+            'name' => $faker->unique()->firstName,
+            'email' => $faker->unique()->safeEmail,
+            'password' => bcrypt('test'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
