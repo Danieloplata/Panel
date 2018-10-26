dd87ff1b7d711d7e21e397e996d82243772bd353
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2018-10-26 15:48:28 +0100

    Added note

--- database/seeds/ProjectsWithPanelsSeeder.php
+++ database/seeds/ProjectsWithPanelsSeeder.php
@@ -17,6 +17,7 @@ class ProjectsWithPanelsSeeder extends Seeder
 			$panels = factory('App\Panel', 3)->create(['project_id' => $project->id, 'user_id' => $project->user_id]);
 			$provider = factory('App\Provider')->create();
 
+            // each(function ($panel) use ($project, $provider)
 			$panels->each(function($panel) {
 				$respondents = factory('App\Respondent', 200)->create(['panel_id' => $panel->id]);
 			});
