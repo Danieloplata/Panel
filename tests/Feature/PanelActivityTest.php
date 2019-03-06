c2ef1bed16f2c3eaf24f21c35192b062fdd16df2
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-03-06 17:24:02 +0000

    Added panel observer and tests

---
+++ tests/Feature/PanelActivityTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PanelActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp() 
+    {
+        parent::setup();
+        $this->be($user = factory('App\User')->create());
+        $this->project = factory('App\Project')->create();
+        $this->panel = factory('App\Panel')->create(['project_id' => $this->project->id]);
+        $this->provider = factory('App\Provider')->create(['panel_id' => $this->panel->id]);
+    }
+
+    /** @test */
+    function creating_a_panel_generates_activity()
+    {
+        $this->withoutExceptionHandling();
+        
+        $this->assertCount(2, $this->panel->project->activity);
+        $this->assertEquals('panel_created', $this->panel->project->activity->last()->description);
+    }
+
+    /** @test */
+    // function updating_a_panel_generates_activity()
+    // {
+    //     $this->project->update(['projectName' => 'changed']);
+
+    //     $this->assertCount(2, $this->project->activity);
+    //     $this->assertEquals('updated', $this->project->activity->last()->description);
+    // }
+
+}
