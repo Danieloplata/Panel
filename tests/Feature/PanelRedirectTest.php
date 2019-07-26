cc49e4d80d5ba6c01ec0fb0a8cd82cd20e7f96c6
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-07-26 15:20:19 +0100

    Created PanelRedirectTest

--- tests/Feature/PanelRedirectTest.php
+++ tests/Feature/PanelRedirectTest.php
@@ -3,18 +3,34 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PanelRedirectTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setup();
+        $this->be($user = factory('App\User')->create());
+        $this->project = factory('App\Project')->create();
+        $this->panel = factory('App\Panel')->create(['project_id' => $this->project->id]);
+
+        $this->countryPanel = factory('App\CountryPanel')->create([
+            'panel_id' => $this->panel->id,
+            'country_id' => 231
+        ]);
     }
+
+    /** @test */
+    public function a_respondent_can_access_the_start_route()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->get(route('redirectStart', [
+            'panelid' => $this->panel->id,
+            'respondentid' => 'testRespondent'
+        ]))->assertRedirect($this->panel->redirectLink . 'testRespondent');
+    }
+
 }
