99b96689ddec194eb10b466f96acab912f7629eb
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-07-26 15:20:53 +0100

    CountryCheck is currently bypassed until issues are resolved

--- app/Http/Middleware/CountryCheck.php
+++ app/Http/Middleware/CountryCheck.php
@@ -21,24 +21,24 @@ class CountryCheck
         $allowedCountries = $panel->countries->pluck('countryCode')->toArray();
 
         // Replace this with CloudFlare, use CF headers for it
-        $curl = curl_init();
+        //$curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "http://ip-api.com/json/".$_SERVER['REMOTE_ADDR'],
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_TIMEOUT => 30000,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-            ),
-        ));
-        
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        $geoloc = json_decode($response);
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => "http://ip-api.com/json/".$_SERVER['REMOTE_ADDR'],
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => "",
+//            CURLOPT_TIMEOUT => 30000,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => "GET",
+//            CURLOPT_HTTPHEADER => array(
+//                'Content-Type: application/json',
+//            ),
+//        ));
+//
+//        $response = curl_exec($curl);
+//        $err = curl_error($curl);
+//        curl_close($curl);
+//        $geoloc = json_decode($response);
         //$countryCode = sanitise($geoloc->countryCode);
         $countryCode = "GB";
         session(['countryCode' => $countryCode]);
