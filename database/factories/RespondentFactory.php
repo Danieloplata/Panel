1692955652913a560d30c5b7467cf37d7b6e2372
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-07-26 15:18:41 +0100

    Updated respondent factory to test average completion time works

--- database/factories/RespondentFactory.php
+++ database/factories/RespondentFactory.php
@@ -14,6 +14,6 @@ $factory->define(App\Respondent::class, function (Faker $faker) {
         'userAgent' => $faker->userAgent,
         'status' => $faker->randomElement($array = array ('complete','quotafull','incomplete','screenout')),
         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        'updated_at' => Carbon::now()->addMinutes(5)->format('Y-m-d H:i:s')
     ];
 });
